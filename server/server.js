ea18cac62c219c198498b5322f68962568248523
Abhishek-raj093 <ss0986825@gmail.com>
2025-05-10 08:49:30 +0530

    UserController Camplet

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 // Create Express app and HTTP Server
 const app = express();
@@ -15,7 +16,9 @@ app.use(express.json({limit: "4mb"}));
 // Connect to MongoDB
 await connectDB();
 
+// ROutes setup
 app.use("/api/status", (req, res) => res.send("Server is live"));
+app.use("/api/auth", userRouter)
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log("Server is running on PORT:" + PORT));
